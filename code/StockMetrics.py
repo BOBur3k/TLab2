88239a8b238a128dfe2986632aa0b3e21a0dec68
Bob <babur3000b@gmail.com>
2023-10-29 11:59:40 -0400

    Adding

--- code/StockMetrics.py
+++ code/StockMetrics.py
@@ -1,4 +1,18 @@
-def average01(self):
+
+import statistics as stats
+
+from code.StockData import StockData
+
+class StockMetrics(StockData):
+    def __init__(self, path):
+        # call the parent method's constructor
+        super(StockMetrics, self).__init__(path)
+
+        # now that we've ran self.load(), we can interact with "self.data" as a
+        # list of lists
+        self.load()
+
+    def average01(self):
         
         averages = []
         for row in self.data:
